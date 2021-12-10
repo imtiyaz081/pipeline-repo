78e9add06adfc08b4b375f49682268a036b8db69
imtiyaz <imtiyaz081@gmail.com>
2021-12-10 17:15:12 +0000

    this is pipeline script

---
+++ jenkinsfile.sh
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+           
+            steps {
+			git 'https://github.com/imtiyaz081/javarepo.git'
+			sh 'mvn clean install'
+                echo "this is build stage"
+            }
+        }
+		stage('deploy') {
+           
+            steps {
+                echo "this is deploy stage"
+            }
+        }
+	stage('test') {
+           
+            steps {
+                echo "this is test stage"
+            }
+        }	
+    }
+}
